77f0818529e391c47206f780efe065ef9a30a571
Wahyu101101 <whyuhdyat101@gmail.com>
2026-06-04 10:44:50 +0700

    Fix VIEW_COMPILED_PATH to point to /tmp/storage/framework/views

--- barber-backend/api/index.php
+++ barber-backend/api/index.php
@@ -5,13 +5,13 @@ putenv('APP_EVENTS_CACHE=/tmp/events.php');
 putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
 putenv('APP_ROUTES_CACHE=/tmp/routes.php');
 putenv('APP_SERVICES_CACHE=/tmp/services.php');
-putenv('VIEW_COMPILED_PATH=/tmp');
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
 $_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
 $_ENV['APP_EVENTS_CACHE'] = '/tmp/events.php';
 $_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
 $_ENV['APP_ROUTES_CACHE'] = '/tmp/routes.php';
 $_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
-$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
 
 $storagePath = '/tmp/storage';
 if (!is_dir($storagePath)) {
