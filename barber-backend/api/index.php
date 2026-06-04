b79d2e505b2b5dd56538571a0216545338663a21
Wahyu101101 <whyuhdyat101@gmail.com>
2026-06-04 10:06:11 +0700

    Create storage subdirectories in /tmp

--- barber-backend/api/index.php
+++ barber-backend/api/index.php
@@ -1,3 +1,13 @@
 <?php
+$storagePath = '/tmp/storage';
+if (!is_dir($storagePath)) {
+    mkdir($storagePath, 0777, true);
+    mkdir($storagePath.'/framework/views', 0777, true);
+    mkdir($storagePath.'/framework/cache', 0777, true);
+    mkdir($storagePath.'/framework/cache/data', 0777, true);
+    mkdir($storagePath.'/framework/sessions', 0777, true);
+    mkdir($storagePath.'/logs', 0777, true);
+}
+
 // Forward Vercel requests to normal Laravel public/index.php
 require __DIR__ . '/../public/index.php';
