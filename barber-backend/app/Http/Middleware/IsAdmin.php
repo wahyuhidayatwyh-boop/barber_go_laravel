9cae62f67c0724aa8cd7406bd66754a6e101a16c
Wahyu101101 <whyuhdyat101@gmail.com>
2026-06-04 10:47:57 +0700

    Fix case sensitivity for IsAdmin middleware

--- barber-backend/app/Http/Middleware/IsAdmin.php
+++ barber-backend/app/Http/Middleware/IsAdmin.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
-class isAdmin
+class IsAdmin
 {
     /**
      * Handle an incoming request.
